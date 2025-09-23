fbc63eddf426c899ef739283c776d436b91c147d
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-09-23 12:55:45 -0600

    Refactor code to include new zod schema for admin request

--- src/types/newRequestSchema.ts
+++ src/types/newRequestSchema.ts
@@ -20,3 +20,25 @@ export const newRequestSchema = z.object({
 });
 
 export type NewRequestSchema = z.infer<typeof newRequestSchema>;
+
+export const newAdminRequestSchema = z.object({
+  userId: z.string().min(1, "User selection is required"),
+  serviceDayId: z.string().min(1, "Service selection is required"),
+  requestDate: z.string().refine(
+    (date) => {
+      const now = new Date();
+      const selectedDate = new Date(date);
+      return (
+        selectedDate.toString() !== "Invalid Date" &&
+        selectedDate >= new Date(now.toDateString())
+      );
+    },
+    {
+      message: "Please select a valid date that is today or later",
+    }
+  ),
+  addressId: z.string().min(1, "Pickup address is required"),
+  notes: z.string().optional(),
+});
+
+export type NewAdminRequestSchema = z.infer<typeof newAdminRequestSchema>;
