214ee901bc5cd8ed4bcb23d678014011e51d05dd
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-09-24 12:12:58 -0600

    Refactor code for redefining requestDate type

--- src/types/newRequestSchema.ts
+++ src/types/newRequestSchema.ts
@@ -2,14 +2,11 @@ import { z } from "zod";
 
 export const newRequestSchema = z.object({
   serviceDayId: z.string().min(1, "Service selection is required"),
-  requestDate: z.string().refine(
+  requestDate: z.date().refine(
     (date) => {
       const now = new Date();
-      const selectedDate = new Date(date);
-      return (
-        selectedDate.toString() !== "Invalid Date" &&
-        selectedDate >= new Date(now.toDateString())
-      );
+      const today = new Date(now.toDateString());
+      return date >= today;
     },
     {
       message: "Please select a valid date that is today or later",
@@ -24,14 +21,11 @@ export type NewRequestSchema = z.infer<typeof newRequestSchema>;
 export const newAdminRequestSchema = z.object({
   userId: z.string().min(1, "User selection is required"),
   serviceDayId: z.string().min(1, "Service selection is required"),
-  requestDate: z.string().refine(
+  requestDate: z.date().refine(
     (date) => {
       const now = new Date();
-      const selectedDate = new Date(date);
-      return (
-        selectedDate.toString() !== "Invalid Date" &&
-        selectedDate >= new Date(now.toDateString())
-      );
+      const today = new Date(now.toDateString());
+      return date >= today;
     },
     {
       message: "Please select a valid date that is today or later",
