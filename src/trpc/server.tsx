b3885f8d5e7f4b29e059a30f6dbeeb33bb348a04
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-10-28 11:56:04 -0600

    Add reusable prefetch and hydration boundary

--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -5,6 +5,18 @@ import { cache } from "react";
 import { createTRPCContext } from "./init";
 import { makeQueryClient } from "./query-client";
 import { appRouter } from "./routers/_app";
+import {
+  dehydrate,
+  FetchInfiniteQueryOptions,
+  FetchQueryOptions,
+  HydrationBoundary,
+  QueryKey,
+} from "@tanstack/react-query";
+
+// Proper type for that includes both regular and infinite query options
+type PrefetchOptions =
+  | FetchQueryOptions<unknown, Error, unknown, QueryKey>
+  | FetchInfiniteQueryOptions<unknown, Error, unknown, QueryKey, unknown>;
 
 export const getQueryClient = cache(makeQueryClient);
 // const caller = createCallerFactory(appRouter)(createTRPCContext);
@@ -15,3 +27,39 @@ export const trpc = createTRPCOptionsProxy({
   router: appRouter,
   queryClient: getQueryClient,
 });
+
+export const HydrateClient = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = getQueryClient();
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {children}
+    </HydrationBoundary>
+  );
+};
+
+export const prefetch = (queryOptions: PrefetchOptions) => {
+  const queryClient = getQueryClient();
+
+  const queryKey = queryOptions.queryKey;
+  const queryKeyMeta = Array.isArray(queryKey)
+    ? (queryKey[1] as { type?: string } | undefined)
+    : undefined;
+  const isInfiniteQuery = queryKeyMeta?.type === "infinite";
+
+  if (isInfiniteQuery) {
+    void queryClient.prefetchInfiniteQuery(
+      queryOptions as FetchInfiniteQueryOptions<
+        unknown,
+        Error,
+        unknown,
+        QueryKey,
+        unknown
+      >
+    );
+  } else {
+    void queryClient.prefetchQuery(
+      queryOptions as FetchQueryOptions<unknown, Error, unknown, QueryKey>
+    );
+  }
+};
