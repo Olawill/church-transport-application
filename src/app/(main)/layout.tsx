e5edef0a3d070afa0134e250175faabf16c920e3
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-12-23 16:46:02 -0700

    Update layout and dependencies

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,21 +8,20 @@ const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await requireAuth();
 
   return (
-    <LayoutBackground>
-      <SidebarProvider defaultOpen={false}>
-        <div className="flex flex-col w-full">
+    <SidebarProvider defaultOpen={false}>
+      <LayoutBackground>
+        <NavAppSidebar initialSession={session} />
+        <SidebarInset className="flex flex-col min-h-screen">
           <Header initialSession={session} />
-          <div className="flex flex-1">
-            <NavAppSidebar initialSession={session} />
-            <SidebarInset className="flex-1">
-              <main className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-                {children}
-              </main>
-            </SidebarInset>
-          </div>
-        </div>
-      </SidebarProvider>
-    </LayoutBackground>
+          {/* Fixed container that adapts to sidebar state */}
+          <main className="flex-1 w-full">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+              {children}
+            </div>
+          </main>
+        </SidebarInset>
+      </LayoutBackground>
+    </SidebarProvider>
   );
 };
 
